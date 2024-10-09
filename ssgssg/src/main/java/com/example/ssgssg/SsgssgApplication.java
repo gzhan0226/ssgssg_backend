99f1a30cceb8e9f5be1e90ac48a96c3781c51327
gzhan0226 <gzhan@naver.com>
2024-10-10 02:31:24 +0900

    chore : 프로젝트 세팅

---
+++ ssgssg/src/main/java/com/example/ssgssg/SsgssgApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssgssg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgssgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgssgApplication.class, args);
+	}
+
+}
